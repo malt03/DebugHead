f4bd884192a57c904efff4aba2cf0c2713286882
Koji Murata <koji.murata@dena.com>
2016-05-28 03:56:15 +0900

    update DebugMenuExit

--- DebugHead/Classes/DebugMenuExit.swift
+++ DebugHead/Classes/DebugMenuExit.swift
@@ -7,12 +7,12 @@
 //
 
 public class DebugMenuExit: DebugMenu {
-  public static let debugMenuTitle = "exit"
+  public static let debugMenuTitle = "Exit"
   public static let debugMenuDangerLevel = DebugMenuDangerLevel.Extreme
   public static let debugMenuAccessoryType = UITableViewCellAccessoryType.None
   public static func debugMenuSelected(debugMenuTableViewController: UITableViewController) -> UIViewController? {
     let alert = UIAlertController(title: "Exit", message: nil, preferredStyle: .Alert)
-    alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in
+    alert.addAction(UIAlertAction(title: "OK", style: .Destructive) { _ in
       exit(1)
     })
     alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
