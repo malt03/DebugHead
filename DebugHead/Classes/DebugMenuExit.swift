9eba4c011f2524f5f71b6ec594b471b984336fe8
Koji Murata <koji.murata@dena.com>
2016-05-28 03:55:08 +0900

    DebugMenuExit

---
+++ DebugHead/Classes/DebugMenuExit.swift
@@ -0,0 +1,22 @@
+//
+//  DebugMenuExit.swift
+//  Pods
+//
+//  Created by Koji Murata on 2016/05/28.
+//
+//
+
+public class DebugMenuExit: DebugMenu {
+  public static let debugMenuTitle = "exit"
+  public static let debugMenuDangerLevel = DebugMenuDangerLevel.Extreme
+  public static let debugMenuAccessoryType = UITableViewCellAccessoryType.None
+  public static func debugMenuSelected(debugMenuTableViewController: UITableViewController) -> UIViewController? {
+    let alert = UIAlertController(title: "Exit", message: nil, preferredStyle: .Alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in
+      exit(1)
+    })
+    alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+    debugMenuTableViewController.presentViewController(alert, animated: true, completion: nil)
+    return nil
+  }
+}
