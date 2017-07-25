7b5d33369546044b620a85799675fc9eb7c8f04b
Koji Murata <koji.murata@dena.com>
2017-07-25 14:09:10 +0900

    carthage project

---
+++ Carthage/DebugHead/DebugHead/DebugHead.h
@@ -0,0 +1,19 @@
+//
+//  DebugHead.h
+//  DebugHead
+//
+//  Created by Koji Murata on 2017/07/25.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DebugHead.
+FOUNDATION_EXPORT double DebugHeadVersionNumber;
+
+//! Project version string for DebugHead.
+FOUNDATION_EXPORT const unsigned char DebugHeadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebugHead/PublicHeader.h>
+
+
