84899412219e2a30df4df4b50d0f17c29d0fe6c2
Koji Murata <koji.murata@dena.com>
2017-07-25 12:38:50 +0900

    remove example/pods

--- Example/Pods/Target Support Files/BugImageCreator/BugImageCreator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BugImageCreator : NSObject
-@end
-@implementation PodsDummy_BugImageCreator
-@end
--- Example/Pods/Target Support Files/DebugHead/DebugHead-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DebugHead : NSObject
-@end
-@implementation PodsDummy_DebugHead
-@end
